c1c365c093c0f378457faffeba84bc8d1583617e
michaelsizonenko <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>
2021-09-06 15:30:14 +0300

    test_fix_dict

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ from schemas import User
 
 app = FastAPI()
 
-users_list = {}
+user_db = {}
 
 
 @app.get("/user", status_code=200)
 def read_all(response: Response):
-    if len(users_list) > 0:
-        return {users_list[key].get('name') for key in users_list}
+    if len(user_db) > 0:
+        return {user_db[key].get('name') for key in user_db}
     response.status_code = status.HTTP_404_NOT_FOUND
     return {"Msg": "No users in DB"}
 
 
 @app.get("/user/{user_id}", status_code=200)
 def read_user_id(user_id: int, response: Response):
-    if user_id in users_list:
-        return users_list[user_id]
+    if user_id in user_db:
+        return user_db[user_id]
     response.status_code = status.HTTP_404_NOT_FOUND
     return {"Msg": "No users in DB"}
 
 
 @app.put("/user/{user_id}", status_code=200)
 def update_user(user_id: int, user_entity: User, response: Response):
-    if user_id in users_list:
+    if user_id in user_db:
         user_entity.id = user_id
-        users_list[user_id] = dict(user_entity)
+        user_db[user_id] = dict(user_entity)
         return {"Msg": f"user {user_entity.email} get all changes"}
     response.status_code = status.HTTP_400_BAD_REQUEST
     return {"Msg": "User not exist, can't update"}
@@ -34,9 +34,9 @@ def update_user(user_id: int, user_entity: User, response: Response):
 
 @app.post("/user", status_code=201)
 def create_user(user_entity: User, response: Response):
-    if user_entity.email not in [users_list[key].get('email') for key in users_list]:
-        user_entity.id = len(users_list) + 1
-        users_list[user_entity.id] = dict(user_entity)
+    if user_entity.email not in [user_db[key].get('email') for key in user_db]:
+        user_entity.id = len(user_db) + 1
+        user_db[user_entity.id] = dict(user_entity)
         return {"Msg": f"user {user_entity.email} added"}
     response.status_code = status.HTTP_400_BAD_REQUEST
     return {"Msg": "User exist, try another"}
@@ -44,8 +44,8 @@ def create_user(user_entity: User, response: Response):
 
 @app.delete("/user/{user_id}", status_code=200)
 def delete_user(user_id: int, user_entity: User, response: Response):
-    if user_id in users_list:
-        del users_list[user_id]
+    if user_id in user_db:
+        del user_db[user_id]
         return {"Msg": f"user {user_entity.email} delete"}
     response.status_code = status.HTTP_400_BAD_REQUEST
     return {"Msg": "User exist, try another"}
@@ -53,18 +53,18 @@ def delete_user(user_id: int, user_entity: User, response: Response):
 
 @app.patch("/user/{user_id}", status_code=200)
 def patch_user(user_id: int, response: Response, user_entity: User):
-    if user_id in users_list:
-        if users_list[user_id]['name'] != user_entity.name and users_list[user_id]['password'] != user_entity.password:
+    if user_id in user_db:
+        if user_db[user_id]['name'] != user_entity.name and user_db[user_id]['password'] != user_entity.password:
             user_entity.id = user_id
-            users_list[user_id] = dict(user_entity)
+            user_db[user_id] = dict(user_entity)
             return {"Msg": f"user {user_entity.email} changed Name and Password"}
-        if users_list[user_id]['password'] != user_entity.password:
+        if user_db[user_id]['password'] != user_entity.password:
             user_entity.id = user_id
-            users_list[user_id] = dict(user_entity)
+            user_db[user_id] = dict(user_entity)
             return {"Msg": f"user {user_entity.email} changed Password"}
-        if users_list[user_id]['name'] != user_entity.name:
+        if user_db[user_id]['name'] != user_entity.name:
             user_entity.id = user_id
-            users_list[user_id] = dict(user_entity)
+            user_db[user_id] = dict(user_entity)
             return {"Msg": f"user {user_entity.email} changed Name"}
         return {"Msg": "No changes"}
     response.status_code = status.HTTP_404_NOT_FOUND
